e86b90c9aa9bfcfc4c44d2046dfda787abba6dda
avivarma-mdx <avi.varma@perforce.com>
2025-01-06 05:27:59 -0800

    updated to actually work

--- src/company_name/client/main.py
+++ src/company_name/client/main.py
@@ -1,11 +1,19 @@
-from libs.module1 import submodule1, submodule2
-from libs.module2 import submodule3
 import json
 import os
+import sys
+# Option 1
+# export PYTHONPATH=/home/avi.varma/workspace/my_project
+
+# Option 2
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../../../')))
+
+
+from src.company_name.client.module1 import submodule1, submodule2
+from src.company_name.libs.module2 import submodule3
 
 def main():
     # Load configuration
-    config_path = os.path.join(os.path.dirname(__file__), '../config/config.json')
+    config_path = os.path.join(os.path.dirname(__file__), '../../../config/config.json')
     with open(config_path, 'r') as file:
         config = json.load(file)
     
